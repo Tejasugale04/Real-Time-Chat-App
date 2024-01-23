23e540f167ec3650920cad735ce9e1953e5db165
Tejasugale04 <tejasugale04@gmail.com>
2024-01-23 22:07:03 +0530

    made some changes

--- src/main/java/com/springchat/chat_app/user/UserController.java
+++ src/main/java/com/springchat/chat_app/user/UserController.java
@@ -7,35 +7,35 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
-  private  final UserService service;
 
-  @MessageMapping("/user.addUser")
-  @SendTo("/user/topic")
-  public User addUser(
-          @Payload User user
-  ){
-      service.saveUser(user);
-      return user;
-  }
+    private final UserService userService;
 
-  @MessageMapping("/user.disconnectUser")
-  @SendTo("/user/topic")
-  public User disconnect(
-          @Payload User user
-  ){
-      service.disconnect(user);
-      return user;
-  }
+    @MessageMapping("/user.addUser")
+    @SendTo("/user/public")
+    public User addUser(
+            @Payload User user
+    ) {
+        userService.saveUser(user);
+        return user;
+    }
 
-@GetMapping("/users")
-  public ResponseEntity<List<User>> findConnectedUsers(){
-      return ResponseEntity.ok(service.findConnectedUsers());
-  }
+    @MessageMapping("/user.disconnectUser")
+    @SendTo("/user/public")
+    public User disconnectUser(
+            @Payload User user
+    ) {
+        userService.disconnect(user);
+        return user;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> findConnectedUsers() {
+        return ResponseEntity.ok(userService.findConnectedUsers());
+    }
 }
